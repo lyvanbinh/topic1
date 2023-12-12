912aaeba9777d2b25c79536a9cc5975c536353a0
lyvanbinh <binhboong0310@gmail.com>
2023-12-12 22:28:56 +0700

    Đăng nhập và Xét Quyền
    -Thêm trang đăng nhập và thực hiện việc đăng nhập bằng tài khoản mặc định
    
    - Xét user cho làm nhân viên hoặc admin hoặc làm khách hàng thông qua đó xử lí quyền sau này

---
+++ dist/js/main.js
@@ -0,0 +1,93 @@
+
+(function ($) {
+    "use strict";
+
+
+     /*==================================================================
+    [ Focus input ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })
+    })
+
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+
+
+})(jQuery);
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Lấy tham chiếu đến form và các trường input
+    var loginForm = document.querySelector('.login100-form');
+    var usernameInput = document.querySelector('input[name="username"]');
+    var passwordInput = document.querySelector('input[name="pass"]');
+
+    // Xử lý sự kiện khi form được submit
+    loginForm.addEventListener('submit', function (event) {
+      event.preventDefault(); // Ngăn chặn form submit mặc định
+
+      // Kiểm tra tên đăng nhập và mật khẩu
+      if (usernameInput.value === 'admin' && passwordInput.value === 'admin') {
+        // Nếu đúng, chuyển hướng đến trang index.html
+        window.location.href = 'index.html';
+      } else {
+        // Nếu sai, hiển thị thông báo hoặc thực hiện các hành động khác
+        alert('Invalid username or password. Please try again.');
+      }
+    });
+  });
