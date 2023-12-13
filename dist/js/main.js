0e1f282533399ec7061c004fff0f94f9068f3a77
lyvanbinh <binhboong0310@gmail.com>
2023-12-13 10:03:03 +0700

    Hoàn thành
    Chỉnh sửa việc đăng nhập thành công

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,11 +3,11 @@
     "use strict";
 
 
-     /*==================================================================
-    [ Focus input ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
+    /*==================================================================
+   [ Focus input ]*/
+    $('.input100').each(function () {
+        $(this).on('blur', function () {
+            if ($(this).val().trim() != "") {
                 $(this).addClass('has-val');
             }
             else {
@@ -21,13 +21,13 @@
     [ Validate ]*/
     var input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(){
+    $('.validate-form').on('submit', function () {
         var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
                 showValidate(input[i]);
-                check=false;
+                check = false;
             }
         }
 
@@ -35,20 +35,20 @@
     });
 
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
         });
     });
 
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
         }
         else {
-            if($(input).val().trim() == ''){
+            if ($(input).val().trim() == '') {
                 return false;
             }
         }
@@ -70,7 +70,6 @@
 
 })(jQuery);
 
-
 document.addEventListener('DOMContentLoaded', function () {
     // Lấy tham chiếu đến form và các trường input
     var loginForm = document.querySelector('.login100-form');
@@ -79,15 +78,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Xử lý sự kiện khi form được submit
     loginForm.addEventListener('submit', function (event) {
-      event.preventDefault(); // Ngăn chặn form submit mặc định
+        event.preventDefault(); // Ngăn chặn form submit mặc định
 
-      // Kiểm tra tên đăng nhập và mật khẩu
-      if (usernameInput.value === 'admin' && passwordInput.value === 'admin') {
-        // Nếu đúng, chuyển hướng đến trang index.html
-        window.location.href = 'index.html';
-      } else {
-        // Nếu sai, hiển thị thông báo hoặc thực hiện các hành động khác
-        alert('Invalid username or password. Please try again.');
-      }
+        // Kiểm tra tên đăng nhập và mật khẩu
+        if (usernameInput.value === 'admin' && passwordInput.value === 'admin') {
+            // Nếu đúng, chuyển hướng đến trang index.html
+            window.location.href = 'index.html';
+        } else {
+            // Nếu sai, hiển thị thông báo hoặc thực hiện các hành động khác
+            alert('Invalid username or password. Please try again.');
+        }
     });
-  });
+});
+
+
+
